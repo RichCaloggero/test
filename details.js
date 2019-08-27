20694d885b0fbaee43d09460ac7da36b7912ce66
Rich Caloggero <rjc@mit.edu>
2019-08-27 12:04:42 -0400

    * updated details-webcomponent
    - no-heading removes heading markup from collapsible-list (useful for top-level single node as in .html example)
    - permissively wraps non list items in "li"
    - adds button elements if "li" contains data-action attribute
    - adds "a" element if "li" contains "href" attribute
    - should be able to specify starting level for headings via attribute, but doesn't work at the moment

--- details.js
+++ details.js
@@ -9,45 +9,55 @@ return ["data-label"];
 
 constructor () {
 super ();
-this.root = this.attachShadow({mode: "open"});
 } // constructor
 
 connectedCallback () {
 //console.debug (`list connected:\n${this.innerHTML}`);
 const list = document.createElement("ul");
-const listItem = document.createElement("li");
 const parent = this.parentElement;
-if (!this.level) this.level = initialLevel;
+const noHeading = this.hasAttribute("no-heading");
+if (!this.level) {
+console.log(`level is ${this.getAttribute("level")}`);
+this.level = this.hasAttribute("level")? Number(this.getAttribute("level")) : initialLevel;
+} // if
 
 Array.from(this.children).forEach(child => {
-child.level = this.level+1;
+if (!noHeading) child.level = this.level+1;
 
-if (child.tagName.toLowerCase() === "li") {
+if (child.matches("li")) {
  if (child.hasAttribute("data-href")) {
 const hRef = child.getAttribute("data-href");
 child.innerHTML = `<a href="${hRef}">${child.textContent}</a>`;
+child.removeAttribute("data-href");
 
 } else if (child.hasAttribute("data-action")) {
 const action = child.getAttribute("data-action");
 child.innerHTML = `<button data-action="${action}">${child.textContent}</button>`;
+child.removeAttribute("data-action");
 } // if
+
+} else {
+const listItem = document.createElement("li");
+listItem.appendChild(child);
+child = listItem;
 } // if
 
 list.appendChild(child)
 });
+
 let container = list;
 if (this.hasAttribute("data-label")) {
 const labelText = this.getAttribute("data-label");
-const top = parent.nodeName.toLowerCase() !== this.nodeName.toLowerCase();
 const details = document.createElement("details");
-details.innerHTML = `<summary><span role="heading" aria-level=${this.level.toString()}>${labelText}</span></summary>`;
+details.innerHTML = noHeading?
+`<summary>${labelText}</summary>`
+: `<summary><span role="heading" aria-level=${this.level.toString()}>${labelText}</span></summary>`;
 details.setAttribute("role", "presentation");
 details.appendChild(list);
 container = details;
 } // if
 
-listItem.appendChild(container);
-parent.replaceChild(listItem, this);
+parent.replaceChild(container, this);
 } // connectedCallback 
 
 attributeChanged (name, value) {
