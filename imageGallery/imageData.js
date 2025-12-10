02483c7c6cf61f29214b603bc7bd705b01c06ebd
Rich Caloggero <rjc@mit.edu>
2025-12-10 18:58:22 -0500

    * added imageGallery demo

---
+++ imageGallery/imageData.js
@@ -0,0 +1,22 @@
+export const imageData = [
+{path: "images/image1.jpg",
+alt: "Site overview",
+url: "https://ocw.mit.edu/courses/12-114-field-geology-i-fall-2005/b899ef9ba2b7e809c93c929e0c88dd8b_lec1photo1.jpg",
+description: `<p>We do our field work in the southwest United States in January. It is true that there is snow in the high mountains in the desert at that time but we will not work that high. We try to work in snow-free regions. There is a lot of walking. As the regions we are mapping are often not accessible by car, we will walk in and out each day so it is important to be in good shape.
+</p>`
+
+}, {
+alt: "Camp setup",
+path: "images/image2.jpg",
+url: "https://ocw.mit.edu/courses/12-114-field-geology-i-fall-2005/5ac87135220dc49668e1d66b481ea651_lec1photo2.jpg",
+description: `<p>This is an image of the camp set-up. There is a cook tent in the background. Inside are stoves, tables, food storage and lanterns for the evenings. We wash our own dishes and you can see them drying on the table. There is also a cooler for our veggies and drinks. As we are at a remote location in the desert, water is scarce. The red water cooler is for drinking during the day, but to be conserved when washing. There will be three showers during IAP or about once a week or 10 days. There will be mapping groups that will also have roles in the kitchen. These will alternate day-to-day, but each group will have responsibilities to help in food preparation and in washing kitchen dishes.
+</p>`
+
+}, {
+alt: "map tent",
+path: "images/image3.jpg",
+url: "https://ocw.mit.edu/courses/12-114-field-geology-i-fall-2005/cfae833c082c87017c120fcaff26d078_lec1photo3.jpg",
+description: `<p>This is the map tent where we store the computers and the mapping equipment. It is where we will eat if the weather is bad but most of the time we will use this tent for doing the digital mapping work at night. We have generators and batteries for electricity but will use gas lanterns for light. These tents are high quality arctic tents and have served us well. We purchased them with gracious support from the Kaplan Foundation. There is a lot of wind in the desert, especially at night and, our old tents were knocked over frequently.
+</p>`
+}]
+
---
+++ imageGallery/imageGallery.js
@@ -0,0 +1,36 @@
+import { imageData } from "./imageData.js";
+
+document.body.insertAdjacentHTML("beforeEnd", `
+<dialog popover id="image-display">
+<div><button autofocus popoverTarget="image-display" popoverAction="hide">Close</button></div>
+<div aria-hidden="true"><img class="image" src="" alt=""></div>
+<div class="long-description"></div>
+`);
+
+createThumbnails (imageData, document.querySelector(".image-gallery"));
+document.querySelector("#image-display").addEventListener("beforetoggle", updateImageDisplay);
+
+function createThumbnails (imageData, gallery) {
+for (const data of imageData) {
+gallery.insertAdjacentHTML("beforeEnd", `
+<li><button popoverTarget="image-display" popoverAction="show">
+<img
+alt="${data.alt || "decorative"}"
+src="${data.url? data.url : data/path}"
+data-description="${data.description}"
+></button></li>
+`);
+} // for
+} // importImageData
+
+function updateImageDisplay (e) {
+console.log("before popup ", e.target, e);
+const thumbnail = e.source.querySelector("img");
+const descriptionText = thumbnail.dataset.description;
+
+const longDescription = e.target.querySelector(".long-description");
+const image = e.target.querySelector(".image");
+
+image.src = thumbnail.src;
+longDescription.innerHTML = descriptionText;
+} // updateImageDisplay
